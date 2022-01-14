7e1d43a0c9ddf04f21935ea1951c17136fb14033
Mateo Lelong <mateo.lelong@gmail.com>
2022-01-14 15:29:24 +0100

    Create folders and FileReader class

---
+++ CubeTools/FileManager/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubeTools.FileManager
+{
+    internal class FileReader
+    {
+        bool IsFile(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}
