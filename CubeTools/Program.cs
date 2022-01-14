9c0229046bce68ceb12f2898ee19ac0689f32984
Mateo Lelong <mateo.lelong@gmail.com>
2022-01-14 15:12:48 +0100

    hello test en IP

--- CubeTools/Program.cs
+++ CubeTools/Program.cs
@@ -7,6 +7,7 @@ namespace CubeTools
 {
     internal class Program
     {
+        // HELLLLLLLLLO WORLD
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
