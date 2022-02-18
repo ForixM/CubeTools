803252e6b4805eb04b3d04d46ddfec1c5a775cd5
Max SAILLY <max.sailly@epita.fr>
2022-02-18 12:08:40 +0100

    Added Btn Fonction + ListBox Xaml part

--- UI/ViewModels/ViewModelBase.cs
+++ UI/ViewModels/ViewModelBase.cs
@@ -7,5 +7,109 @@ namespace UI.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
+        public void LeftBtnClick()
+        {
+
+        }
+
+        public void RightBtnClick()
+        {
+
+        }
+
+        public void UpBtnClick()
+        {
+
+        }
+
+        public void SyncBtnClick()
+        {
+
+        }
+
+        public void SettingBtnClick()
+        {
+
+        }
+
+        public void CreateBtnClick()
+        {
+
+        }
+
+        public void CopyBtnClick()
+        {
+
+        }
+
+        public void CutBtnClick()
+        {
+
+        }
+
+        public void PasteBtnClick()
+        {
+
+        }
+
+        public void RenameBtnClick()
+        {
+
+        }
+
+        public void DeleteBtnClick()
+        {
+
+        }
+
+        public void NearbySendBtnClick()
+        {
+
+        }
+
+        public void SearchBtnClick()
+        {
+
+        }
+
+        public void GoToHomeBtnClick()
+        {
+
+        }
+
+        public void GoToDesktopBtnClick()
+        {
+
+        }
+
+        public void GoToDocumentsBtnClick()
+        {
+
+        }
+
+        public void GoToDownloadBtnClick()
+        {
+
+        }
+
+        public void GoToPictureBtnClick()
+        {
+
+        }
+
+        public void GoToMusicBtnClick()
+        {
+
+        }
+
+        public void GoToTrashBtnClick()
+        {
+
+        }
+
+        public void GoToSubFile()
+        {
+
+        }
     }
 }
