0f85af5aea5e5c667d40aee32b6db08d6ac32064
Forix <mehdi.el-alaoui-el-aoufoussi@epita.fr>
2022-03-06 15:50:15 +0100

    Implemented Extraction methods and commented all function in Compression.cs

--- Extensions/Compression.cs
+++ Extensions/Compression.cs
@@ -12,6 +12,10 @@ namespace CubeTools
     public class Compression
     {
         private static bool _initialized = false;
+
+        /// <summary>
+        /// Initializer for compression/decompression functionalities
+        /// </summary>
         public static void Init()
         {
             var path = @"C:\Users\forix\Documents\CubeTools\Extensions\bin\Debug\net5.0\Assets\7z.dll";
@@ -20,6 +24,15 @@ namespace CubeTools
             _initialized = true;
         }
 
+        /// <summary>
+        /// Compress the given directory and all his sub-contents into a arvhie file with the same name that the
+        /// directory name in the same path of the directory.
+        /// </summary>
+        /// <param name="directory">Directory to compress</param>
+        /// <param name="archiveFormat">Compression algorithm to use. Only ZIP and SevenZip are supported</param>
+        /// <param name="compressingEvent">Optional method parameter to track compression progression</param>
+        /// <param name="finishedEvent">Optional method parameter called when compression have finished</param>
+        /// <returns>Compression Task which is async and already ran</returns>
         public static Task CompressDirectory(DirectoryType directory, OutArchiveFormat archiveFormat,
             Action<object, ProgressEventArgs> compressingEvent = null, Action<object, EventArgs> finishedEvent = null)
         {
@@ -40,6 +53,15 @@ namespace CubeTools
             return CompressDirectory(directory, dest, archiveFormat, compressingEvent, finishedEvent);
         }
 
+        /// <summary>
+        /// Compress the given directory and all his sub-contents into the given destination
+        /// </summary>
+        /// <param name="directory">Directory to compress</param>
+        /// <param name="dest">File where the directory will be compressed</param>
+        /// <param name="archiveFormat">Compression algorithm to use. Only ZIP and SevenZip are supported</param>
+        /// <param name="compressingEvent">Optional method parameter to track compression progression</param>
+        /// <param name="finishedEvent">Optional method parameter called when compression have finished</param>
+        /// <returns>Compression Task which is async and already ran</returns>
         public static Task CompressDirectory(DirectoryType directory, string dest, OutArchiveFormat archiveFormat,
             Action<object, ProgressEventArgs> compressingEvent = null, Action<object, EventArgs> finishedEvent = null)
         {
@@ -56,6 +78,15 @@ namespace CubeTools
             return compressor.CompressDirectoryAsync(directory.Path, dest);
         }
 
+        /// <summary>
+        /// Compress the given files table into an archive file with the same name that the parent directory of the
+        /// first file in the table
+        /// </summary>
+        /// <param name="files">The FileType table to compress</param>
+        /// <param name="archiveFormat">Compression algorithm to use. Only ZIP and SevenZip are supported</param>
+        /// <param name="compressingEvent">Optional method parameter to track compression progression</param>
+        /// <param name="finishedEvent">Optional method parameter called when compression have finished</param>
+        /// <returns>Compression Task which is async and already ran</returns>
         public static Task CompressFiles(FileType[] files, OutArchiveFormat archiveFormat,
             Action<object, ProgressEventArgs> compressingEvent = null, Action<object, EventArgs> finishedEvent = null)
         {
@@ -77,7 +108,16 @@ namespace CubeTools
             
             return CompressFiles(files, path, archiveFormat, compressingEvent, finishedEvent);
         }
-        
+
+        /// <summary>
+        /// Compress the given files table into the given archive destination
+        /// </summary>
+        /// <param name="files">The FileType table to compress</param>
+        /// <param name="dest">The archive destination where all files will be compressed</param>
+        /// <param name="archiveFormat">Compression algorithm to use. Only ZIP and SevenZip are supported</param>
+        /// <param name="compressingEvent">Optional method parameter to track compression progression</param>
+        /// <param name="finishedEvent">Optional method parameter called when compression have finished</param>
+        /// <returns>Compression Task which is async and already ran</returns>
         public static Task CompressFiles(FileType[] files, string dest, OutArchiveFormat archiveFormat,
             Action<object, ProgressEventArgs> compressingEvent = null, Action<object, EventArgs> finishedEvent = null)
         {
@@ -98,6 +138,46 @@ namespace CubeTools
             }
 
             return compressor.CompressFilesAsync(dest, filePaths);
+        }
+
+        /// <summary>
+        /// Extract all the content of the given archive to a directory with the same name of the archive in the same
+        /// path.
+        /// </summary>
+        /// <param name="archive">The archive path</param>
+        /// <param name="compressingEvent">Optional method parameter to track extraction progression</param>
+        /// <param name="finishedEvent">Optional method parameter called when extraction have finished</param>
+        /// <returns>Extraction Task which is async and already ran</returns>
+        public static Task Extract(FileType archive, Action<object, ProgressEventArgs> compressingEvent = null,
+            Action<object, EventArgs> finishedEvent = null)
+        {
+            string dest = archive.Path;
+            while (dest[dest.Length - 1] != '.')
+            {
+                dest = dest.Remove(dest.Length - 1, 1);
+            }
+            dest = dest.Remove(dest.Length - 1, 1);
+            dest = dest.Replace("/", @"\");
+            return Extract(archive, dest, compressingEvent, finishedEvent);
+        }
+
+        /// <summary>
+        /// Extract all the content of the given archive to the given archive path
+        /// </summary>
+        /// <param name="archive">The archive path</param>
+        /// <param name="destination">The archive destination path</param>
+        /// <param name="compressingEvent">Optional method parameter to track extraction progression</param>
+        /// <param name="finishedEvent">Optional method parameter called when extraction have finished</param>
+        /// <returns>Extraction Task which is async and already ran</returns>
+        public static Task Extract(FileType archive, string destination,
+            Action<object, ProgressEventArgs> compressingEvent = null, Action<object, EventArgs> finishedEvent = null)
+        {
+            destination = destination.Replace("/", @"\");
+            SevenZipExtractor extractor = new SevenZipExtractor(archive.Path);
+            if (compressingEvent != null) extractor.Extracting += new EventHandler<ProgressEventArgs>(compressingEvent);
+            if (finishedEvent != null) extractor.ExtractionFinished += new EventHandler<EventArgs>(finishedEvent);
+            Console.WriteLine(destination);
+            return extractor.ExtractArchiveAsync(destination);
         }
     }
 }
