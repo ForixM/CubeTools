59d820a73d8c7e7a3b7f89de91f228c07ec40d16
Max SAILLY <max.sailly@epita.fr>
2022-03-08 19:07:45 +0100

    Added OAuth and started file manager

---
+++ GoogleDriveApi/ManagerFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+
+namespace GoogleDriveApi
+{
+    internal class ManagerFile
+    {
+    }
+}
--- GoogleDriveApi/Program.cs
+++ GoogleDriveApi/Program.cs
@@ -17,34 +17,15 @@ namespace GoogleDriveApi
     {
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/drive-dotnet-quickstart.json
-        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string[] Scopes = { DriveService.Scope.DriveAppdata };
         static string ApplicationName = "Drive API .NET Quickstart";
 
         static void Main(string[] args)
         {
-            UserCredential credential;
+            UserCredential credential = OAuth.GetUserCredential();
 
-            using (var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
-            {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
-
-            // Create Drive API service.
-            var service = new DriveService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
+            var service = OAuth.GetDriveService(credential);
+            
 
             // Define parameters of request.
             FilesResource.ListRequest listRequest = service.Files.List();
