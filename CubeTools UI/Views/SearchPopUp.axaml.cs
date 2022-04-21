dfbb3a640171667fb42d6ced911beb96086732e5
kevinjmt <keke.jamet@gmail.com>
2022-04-21 17:13:26 +0200

    Added research

---
+++ CubeTools UI/Views/SearchPopUp.axaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using CubeTools_UI.ViewModels;
+using Library.ManagerReader;
+
+namespace CubeTools_UI.Views
+{
+    public class SearchPopUp : Window
+    {
+        private ActionBarViewModel ParentViewModel;
+
+        private TextBox TextEntered;
+        
+        public SearchPopUp(ActionBarViewModel vm)
+        {
+            InitializeComponent();
+            ParentViewModel = vm;
+            TextEntered = this.FindControl<TextBox>("TextEntered");
+        }
+        
+        public SearchPopUp()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void Search_OnClick(object? sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                ParentViewModel.ModelXaml.ViewModel.ViewModelPathsBar.AttachedView.ItemsXaml.Items = 
+                                ManagerReader.ListToObservable(ManagerReader.FastSearchByName(ParentViewModel.ModelXaml.ModelNavigationBar.DirectoryPointer.Path, TextEntered.Text, 25).ToList());
+
+            }
+        }
+    }
+}
