c77c27f46d64c9c62cd0d57c2d2421a2efa97c0c
kevinjmt <keke.jamet@gmail.com>
2022-04-22 11:46:39 +0200

    added error views to repo

---
+++ CubeTools UI/Views/PopUps/CriticalErrorPopUp.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CubeTools_UI.Views.PopUps
+{
+    public class CriticalErrorPopUp : Window
+    {
+        public CriticalErrorPopUp()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
