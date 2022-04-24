95e28768c0c1511d92087c5196184a88eba56af5
kevinjmt <keke.jamet@gmail.com>
2022-04-24 17:42:29 +0200

    push to repo

---
+++ CubeTools UI/Views/PopUps/CreatePopUp.axaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using CubeTools_UI.ViewModels;
+using Library.ManagerReader;
+
+namespace CubeTools_UI.Views.PopUps
+{
+    public class CreatePopUp : Window
+    {
+        private ActionBarViewModel? ViewModel;
+        private TextBox TextEntered;
+
+        #region Init
+        
+        public CreatePopUp()
+        {
+            InitializeComponent();
+            TextEntered = this.FindControl<TextBox>("TextEntered");
+            ViewModel = null;
+        }
+        public CreatePopUp(ActionBarViewModel vm) : this()
+        {
+            ViewModel = vm;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+        
+        #endregion
+
+        #region Events
+        
+        private void SearchClick(object? sender, RoutedEventArgs e)
+        {
+            if (sender is Button && ViewModel?.ParentViewModel != null)
+                ViewModel.ParentViewModel.ViewModelPathsBar.ReloadPath(ManagerReader.FastSearchByName(ViewModel.ParentViewModel.ViewModelNavigationBar.DirectoryPointer.Path, TextEntered.Text, 25).ToList());
+            Close();
+        }
+
+        private void SearchEnter(object? sender, KeyEventArgs e)
+        {
+            if (e.Key is Key.Enter && ViewModel?.ParentViewModel != null)
+            {
+                ViewModel.ParentViewModel.ViewModelPathsBar.ReloadPath(ManagerReader
+                    .FastSearchByName(ViewModel.ParentViewModel.ViewModelNavigationBar.DirectoryPointer.Path,
+                        TextEntered.Text, 25).ToList());
+                Close();
+            }
+        }
+        
+        #endregion
+    }
+}
